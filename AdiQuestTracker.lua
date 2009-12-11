259abffbed18f0649d0059e08c6ab26a0aca8b4c
Adirelle <adirelle@tagada-team.net>
2009-12-11 11:18:45 +0100

    Added options to enable/disable the modules.

--- AdiQuestTracker.lua
+++ AdiQuestTracker.lua
@@ -7,28 +7,68 @@ All rights reserverd.
 local addonName, addon = ...
 addon = LibStub('AceAddon-3.0'):NewAddon(addon, addonName)
 
+local DEFAULTS = {
+	profile = {
+		modules = { ['*'] = true },
+	}
+}
+
 function addon:OnInitialize()
-	self.db = LibStub('AceDB-3.0'):New("AdiQuestTrackerDB", {}, true)
-	
+	self.db = LibStub('AceDB-3.0'):New("AdiQuestTrackerDB", DEFAULTS, true)
+	self.db.RegisterCallback(self, 'OnNewProfile', 'Reboot')
+	self.db.RegisterCallback(self, 'OnProfileChanged', 'Reboot')
+	self.db.RegisterCallback(self, 'OnProfileCopied', 'Reboot')
+	self.db.RegisterCallback(self, 'OnProfileReset', 'Reboot')
+
 	LibStub('AceConfig-3.0'):RegisterOptionsTable(addonName, self.GetOptionsTable)
 	LibStub('AceConfigDialog-3.0'):AddToBlizOptions(addonName, GetAddOnMetadata(addonName, "Title"))
+end
+
+function addon:OnEnable()
+	-- Update module enabled states
+	for name, module in self:IterateModules() do
+		module:SetEnabledState(self.db.profile.module[name])
+	end	
+end
+
+function addon:Reboot()
+	self:Disable()
+	self:Enable()
 end
 
 -- Options
 local options
 function addon.GetOptionsTable()
 	if options then return options end
+	
+	local profileOptions = LibStub('AceDBOptions-3.0'):GetOptionsTable(self.db)
+	profileOptions.order = -1
+	
 	options = {
 		name = GetAddOnMetadata(addonName, "Title"),
 		type = 'group',
 		childGroups = 'tab',
 		args = {
-			profiles = LibStub('AceDBOptions-3.0'):GetOptionsTable(self.db),
+			modules = {
+				name = 'Modules',
+				type = 'multiselect',
+				values = {},
+				get = function(info, key) return addon:GetModule(key):IsEnabled() end,
+				set = function(info, key, value) if value then addon:EnableModule(key) else addon:DisableModule(key) end end,
+				order = 0,
+			},
+			profiles = profileOptions
 		},
 	}
+	
 	for name, module in addon:IterateModules() do
-		options.plugins[name] = module:GetOptionsTable()
+		local mod = module
+		options.args.modules.values[name] = name
+		local modOptions = mod:GetOptionsTable()
+		modOptions.hidden = function() return not mod:IsEnabled() end
+		options.plugins[name] = modOptions
 	end
+	
 	return options
 end
 
@@ -36,8 +76,7 @@ end
 if tekDebug then
 	local debugFrame = tekDebug:GetFrame(addonName)
 	function addon:Debug(...)
-		local prefix = (type(self) == "table" and (self.moduleName or self.name or (type(self.GetName) == "function" and self:GetName()))) or tostring(self)
-		debugFrame:AddMessage(prefix..': '..string.join(", ", tostringall(...)):gsub("([:=]), ", "%1 "))
+		debugFrame:AddMessage('['..tostring(self)..'] '..string.join(", ", tostringall(...)):gsub("([:=]), ", "%1 "))
 	end
 	function addon:HasDebug() return true end
 else
@@ -45,7 +84,7 @@ else
 	function addon:HasDebug() return false end
 end
 
--- Export debug methods to modules
+-- Module methods
 addon:SetDefaultModulePrototype({
 	Debug = addon.Debug,
 	HasDebug = addon.HasDebug,
