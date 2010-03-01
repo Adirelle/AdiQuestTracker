c6bc2ea354826fea6098baec9f044a3867157318
Adirelle <adirelle@tagada-team.net>
2010-03-01 02:13:23 +0100

    Added WatchFrameBackground ; pretty self explicit.

---
+++ modules/WatchFrameBackground.lua
@@ -0,0 +1,42 @@
+--[[
+AdiQuestTracker - a collection of quest tracker enhancement modules.
+(c) 2009 Adirelle (adirelle@tagada-team.net)
+All rights reserverd.
+--]]
+
+local _, core = ...
+local mod = core:NewModule('WatchFrameBackground', 'AceHook-3.0')
+local L = core.L
+
+mod.title = L["Add a background to the WatchFrame."]
+
+local MARGIN = 5
+
+local backdrop
+
+function mod:OnEnable()
+	if not backdrop then
+		backdrop = CreateFrame("Frame", nil, WatchFrameLines)
+		backdrop:SetPoint("TOPLEFT", WatchFrameLines, "TOPLEFT", -24-MARGIN, MARGIN)
+		backdrop:SetPoint("TOPRIGHT", WatchFrameLines, "TOPRIGHT", MARGIN, MARGIN)
+		backdrop:SetBackdrop({bgFile = [[Interface\Tooltips\UI-Tooltip-Background]], tile = true, tileSize = 16})
+		backdrop:SetBackdropColor(0, 0, 0, 0.5)
+		backdrop:SetBackdropBorderColor(0, 0, 0, 0)
+	end
+	self:SecureHook('WatchFrame_Update', 'Update')
+	self:SecureHookScript(WatchFrameLines, 'OnShow', 'Update')
+	self:Update()
+	backdrop:Show()
+end
+
+function mod:OnDisable()
+	backdrop:Hide()
+end
+
+function mod:Update()
+	if WatchFrameLines:IsShown() then
+		self:Debug('Updating size to', WatchFrame.nextOffset)
+		backdrop:SetHeight(-WatchFrame.nextOffset)
+	end
+end
+
