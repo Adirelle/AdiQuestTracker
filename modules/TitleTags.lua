6dc8c8d697aa98f594aa28670bcb8b87c5bf455f
Adirelle <adirelle@tagada-team.net>
2010-03-01 02:11:47 +0100

    Have TitleTags enhance gossip frames ; also color quest tag by difficulty.

--- modules/TitleTags.lua
+++ modules/TitleTags.lua
@@ -5,7 +5,7 @@ All rights reserverd.
 --]]
 
 local _, core = ...
-local mod = core:NewModule('TitleTags', 'AceHook-3.0')
+local mod = core:NewModule('TitleTags', 'AceHook-3.0', 'AceEvent-3.0')
 local L = core.L
 
 mod.title = L["Add level and tags to quest titles"]
@@ -16,21 +16,65 @@ for _, tag in pairs({"Dungeon", "Elite", "Group", "Heroic", "PVP", "Raid"}) do
 end
 
 function mod:OnEnable()
+	self:RegisterEvent('GOSSIP_SHOW', 'ClearTitleCache')
+	self:RegisterEvent('GOSSIP_CLOSED', 'ClearTitleCache')
+	self:RawHook('GetTitleText', 'GetTitleText', true)
 	self:RawHook('GetQuestLogTitle', 'GetQuestLogTitle', true)
+	self:RawHook('GetGossipActiveQuests', 'GetGossipActiveQuests', true)
+	self:RawHook('GetGossipAvailableQuests', 'GetGossipAvailableQuests', true)
 end
 
 function mod:OnDisable()
 	self:UnhookAll()
 end
 
-local function TagTitle(title, level, questTag, suggestedGroup, isHeader, isCollapsed, isComplete, isDaily, ...)
+local function FormatTitle(title, level, tag, isDaily)
+	if title and type(level) == "number" and level > 0 then
+		local color = GetQuestDifficultyColor(level)
+		title = string.format("|cff%02x%02x%02x[%s%s%s]|r %s", 255*color.r, 255*color.g, 255*color.b, (level or ""), tag and tags[tag] or "", isDaily and L['DailyTag'] or "", title)
+	end
+	mod:Debug(title, level, tag, isDaily)
+	return title
+end
+
+local function FormatQuestLogTitle(title, level, questTag, suggestedGroup, isHeader, isCollapsed, isComplete, isDaily, ...)
 	if title and not isHeader then
-		title = string.format("[%d%s%s] %s", level, questTag and tags[questTag] or "", isDaily and L['DailyTag'] or "", title)
+		title = FormatTitle(title, level, questTag, isDaily)
 	end
 	return title, level, questTag, suggestedGroup, isHeader, isCollapsed, isComplete, isDaily, ...
 end
 
 function mod:GetQuestLogTitle(...)
-	return TagTitle(self.hooks.GetQuestLogTitle(...))
+	return FormatQuestLogTitle(self.hooks.GetQuestLogTitle(...))
+end
+
+local gossipTitleCache = {}
+
+local function FormatGossipQuestTitles(title, level, trivial, ...)
+	if not title then return end
+	local newTitle = title
+	if title then
+		newTitle = FormatTitle(title, level)
+		gossipTitleCache[title] = newTitle
+	end
+	mod:Debug('FormatGossipQuestTitles', title, newTitle, level, trivial, more, ...)
+	return newTitle, level, trivial, FormatGossipQuestTitles(...)
+end
+
+function mod:ClearTitleCache()
+	wipe(gossipTitleCache)
+end
+
+function mod:GetGossipAvailableQuests(...)
+	return FormatGossipQuestTitles(self.hooks.GetGossipAvailableQuests(...))
+end
+
+function mod:GetGossipActiveQuests(...)
+	return FormatGossipQuestTitles(self.hooks.GetGossipActiveQuests(...))
+end
+
+function mod:GetTitleText(...)
+	local title = self.hooks.GetTitleText(...)
+	return gossipTitleCache[title or ""] or title
 end
 
